3f3a4abf0a1a7eeb8a6b62f7f534d1718d045fb2
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2011-11-14 23:21:11 +0100

    Versión final para el taller de mañana

---
+++ do-switch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SVN_REPO_BASENAME=$1
+REPO_PATH=$2
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SVN_REPO_URL=file://$SCRIPT_DIR/$SVN_REPO_BASENAME/$REPO_PATH
+
+echo "\$ svn switch file://.../$(basename "$SVN_REPO_BASENAME")/$REPO_PATH"
+svn switch "$SVN_REPO_URL"
---
+++ do-url-copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SVN_REPO_BASENAME=$1
+REPO_PATH_SRC=$2
+REPO_PATH_DST=$3
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SVN_REPO_URL_SRC=file://$SCRIPT_DIR/$SVN_REPO_BASENAME/$REPO_PATH_SRC
+SVN_REPO_URL_DST=file://$SCRIPT_DIR/$SVN_REPO_BASENAME/$REPO_PATH_DST
+
+echo -e \
+"\$ svn cp -m \"Copiado $REPO_PATH_SRC a $REPO_PATH_DST\" \\
+   $SVN_REPO_URL_SRC \\
+   $SVN_REPO_URL_DST"
+svn cp -m "Integrado cambios de $REPO_PATH_SRC en $REPO_PATH_DST" "$SVN_REPO_URL_SRC" "$SVN_REPO_URL_DST"
