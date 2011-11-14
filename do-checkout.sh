b16b6dbb96682becd96de6e88e2d7ea40ee36718
Antonio García <antonio.garciadominguez@uca.es>
2011-11-14 20:34:01 +0100

    Comenzado a añadir la parte de ramas

--- do-checkout.sh
+++ do-checkout.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SVN_REPO_BASENAME=$1
+WORK_DIR=$2
+
 THIS_DIR=$(basename "$(dirname "$(readlink -f "$0")")")
+SVN_REPO_URL=file://$(pwd)/$1/
 
-echo "\$ svn checkout file:///.../$THIS_DIR/svnEjemplo/ $@"
-svn checkout file://$(pwd)/svnEjemplo/ $@
-
+echo "\$ svn checkout file:///.../$THIS_DIR/$SVN_REPO_BASENAME/ $WORK_DIR"
+svn checkout "file://$(pwd)/$SVN_REPO_BASENAME/" "$WORK_DIR"
